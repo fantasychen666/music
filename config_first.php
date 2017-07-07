4da43f7e8c69cd8000113a7381c8c89a5c42ad00
chentong <fantasy_chen666@163.com>
2017-07-07 10:56:08 +0800

    music_commit

---
+++ config_first.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$arr = array();
+$arr[] = "小明";
+$arr[] = "小红";
+echo json_encode($arr);
+
+
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ data.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+
+$hash = $_GET["hash"];
+
+switch($hash){
+			case "first":
+			echo "我是first链接显示";break;
+		case "second":
+			echo "我是second链接显示";break;
+		case "third":
+			echo "我是third链接显示";break;
+}
+
+
+
+
+
+
+
+
+
+ ?>
+
+
+
+
+
+
+
+
